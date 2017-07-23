4561282cb914d4947f10d0250561ed4dd4788adc
bn <borpernic@gmail.com>
2017-07-23 03:21:51 +0300

    HW1 refactor mealscontroller

--- src/main/java/ru/javawebinar/topjava/controller/MealContoller.java
+++ src/main/java/ru/javawebinar/topjava/controller/MealContoller.java
@@ -1,4 +1,4 @@
-package ru.javawebinar.topjava;
+package ru.javawebinar.topjava.controller;
 
 import ru.javawebinar.topjava.model.Meal;
 
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MealContoller extends List<Meal>{
+
     public List<Meal> addMeal(Meal meal);
     public List<Meal> deleteMeal(Meal meal);
     public Meal editMeal(LocalDateTime meal);
