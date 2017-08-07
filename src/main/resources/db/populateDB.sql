ad3dbfb411bbee0a1ec9bdbcd8776eb4e97b4174
bn <borpernic@gmail.com>
2017-08-08 01:41:08 +0300

    3 10 db implementation

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+/*ALTER SEQUENCE global_seq RESTART WITH 100000;*/
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password');
@@ -9,5 +10,13 @@ INSERT INTO users (name, email, password)
 VALUES ('Admin', 'admin@gmail.com', 'admin');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+  ('ROLE_USER', 3),
+  ('ROLE_ADMIN', 4);
+
+INSERT INTO meals (user_id, dateTime, description, calories) VALUES
+  ( 100000,date '2001-09-28','Завтрак', 2000),
+  ( 100000, date '2001-09-28','Обед', 2000),
+  ( 100000, date '2001-09-28','ужин', 2000),
+  ( 100001, date '2001-09-28','Завтрак', 2000),
+  ( 100001,date '2001-09-28','Обед', 2000),
+  ( 100001,date '2001-09-28','ужин', 2000);
